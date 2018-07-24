2d05de7397c8df8bcda393ccf0dd7534a793e8fd
danylenkoivan <danylenkoivan@gmail.com>
2018-07-24 15:01:56 +0200

    Change dev to live setting

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 
-const config = require('./firebase-config/dev.json');
+const config = require('./firebase-config/live.json');
 
 firebase.initializeApp(config);
 
